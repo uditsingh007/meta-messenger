09a150dfd0a01cd64a93026dab5c43d856ac6f30
uditsingh007 <singh.udit14@gmail.com>
2022-11-20 11:09:44 +0530

    Pusher env

--- pusher.ts
+++ pusher.ts
@@ -7,9 +7,9 @@ export const clientPusher = new ClientPusher("26f470ec0a820c6dc616", {
 });
 
 export const serverPusher = new Pusher({
-  appId: "1509053",
-  key: "26f470ec0a820c6dc616",
-  secret: "db905935c24df65789cc",
-  cluster: "ap2",
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
 });
